494b0ded53b4dbe79c2cb504abbe1146c41ba78c
AnaisBZR <anais.bezier@efrei.net>
2025-03-25 11:08:51 +0100

    Initial quotes program

---
+++ quotes.h
@@ -0,0 +1,22 @@
+//
+// Created by anais on 25/03/2025.
+//
+
+#ifndef QUOTES_BEZIER_DARMON_BN_QUOTES_H
+#define QUOTES_BEZIER_DARMON_BN_QUOTES_H
+
+#endif //QUOTES_BEZIER_DARMON_BN_QUOTES_H
+// quotes.h
+#ifndef QUOTES_H
+#define QUOTES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_QUOTES 10
+
+void print_quote(char ** quotes, int index);
+void print_random_quote(char ** quotes);
+
+#endif
